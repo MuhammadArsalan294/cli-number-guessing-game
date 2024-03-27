5189200a9f0784759fa21652fa372d922ee55e54
unknown <pricearslan294@gamil.com>
2024-03-28 04:24:21 +0500

    add the end

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ if (answers.userGuessedNumber === randomNumber) {
     console.log("Congratulations! you guessed right number.");
 }
 else {
-    console.log("you guessed wrong number");
+    console.log("You guessed wrong number");
 }
+// The End
