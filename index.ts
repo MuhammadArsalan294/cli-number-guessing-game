4c08aa1086b61aad8d75d6f2603301b25d29e47e
unknown <pricearslan294@gamil.com>
2024-03-29 03:52:56 +0500

    push my cli number guessing game to github

--- index.ts
+++ index.ts
@@ -1,21 +1,23 @@
 #! /usr/bin/env node
+
 import inquirer from "inquirer";
+
 // 1) Computer will Generate A Random Number
 // 2) User Input For Quessing Number
 // 3) Compare User Input with Computer Generated Number And Show Result
+
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Plase guess a number between 1 to 6: ",
+        message: "Please guess a number between 1 to 6: ",
     },
 ]);
 if (answers.userGuessedNumber === randomNumber) {
     console.log("Congratulations! you guessed right number.");
-}
-else {
+} else {
     console.log("you guessed wrong number");
 }
 
